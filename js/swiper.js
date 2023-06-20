40c5da70640e03d8126d86bcdc2993054b0c27a4
hybf1212 <52036077+hybf1212@users.noreply.github.com>
2023-06-20 11:21:22 +0900

    Add files via upload

---
+++ js/swiper.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper("#bannerSwiper1 .swiper", {
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+        spaceBetween: 30,
+    },
+    effect: "fade",
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
